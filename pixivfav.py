defd3a4819e960954e38cf7ea96388ec88aa5380
txtyb <txtybbill@163.com>
2022-03-15 19:41:42 +0800

    Refactor 'update' and 'send_photo' methods to adapt for sending to multiple chats

--- pixivfav.py
+++ pixivfav.py
@@ -5,6 +5,7 @@ from pixivpy3 import AppPixivAPI
 import os
 import requests
 import yaml
+import json
 
 api = AppPixivAPI()
 
@@ -39,6 +40,7 @@ def replace_char(str):
 # main method
 def update(json_result):
     for illust in json_result.illusts:
+        file_id = None
         # check whether the pic is new
         if os.path.exists(os.path.join('tmp', '%d.jpg')%illust.id) == False:
             api.download(illust.image_urls['large'],
@@ -50,42 +52,92 @@ def update(json_result):
                     replace_char(tag.name), replace_char(tag.name))
             caption = '[%s](https://www.pixiv.net/artworks/%s)\n%s' % (replace_char(illust.title),
                                                                     illust.id, tag_caption)
-            # print sending status
+            # print sending action
             print("Sending......title: %s id: %s" % (illust.title, illust.id))
             sendingstatus = True
             retrytime = 0
-            sendingstatus = send_photo(os.path.join(
-                'tmp', '%s.jpg' % illust.id), c.chat_id, caption=caption)
-            # if sending failed, retry twice
-            while sendingstatus == False and retrytime <= 1:
-                retrytime += 1
-                sleep(1)
-                sendingstatus = send_photo(os.path.join(
-                    'tmp', '%s.jpg' % illust.id), c.chat_id, caption=caption)
-            if retrytime == 2:
-                print('illust.id: %s sending failed after 2 retrys' % illust.id)
-            sleep(1)
+            # send to all the chats
+            for per_chat_id in c.chat_id:
+                # if file_id exist, then use it
+                if file_id != None:
+                    send_action = send_photo(os.path.join('tmp', '%s.jpg' % illust.id), per_chat_id, caption=caption, mode='with_id', file_id=file_id)
+                    sendingstatus = send_action['status']
+                    if sendingstatus:
+                        file_id = send_action['file_id']
+                    # if sending failed, retry twice
+                    while sendingstatus == False and retrytime <= 1:
+                        retrytime += 1
+                        sleep(1)
+                        send_action = send_photo(os.path.join('tmp', '%s.jpg' % illust.id), per_chat_id, caption=caption)
+                        sendingstatus = send_action['status']
+                        if sendingstatus:
+                            file_id = send_action['file_id']
+                    if retrytime == 2:
+                        print('illust.id: %s sending failed after 2 retrys' % illust.id)
+                    sleep(1)
+                else:
+                    send_action = send_photo(os.path.join('tmp', '%s.jpg' % illust.id), per_chat_id, caption=caption)
+                    sendingstatus = send_action['status']
+                    if sendingstatus:
+                        file_id = send_action['file_id']
+                    # if sending failed, retry twice
+                    while sendingstatus == False and retrytime <= 1:
+                        retrytime += 1
+                        sleep(1)
+                        send_action = send_photo(os.path.join('tmp', '%s.jpg' % illust.id), per_chat_id, caption=caption)
+                        sendingstatus = send_action['status']
+                        if sendingstatus:
+                            file_id = send_action['file_id']
+                    if retrytime == 2:
+                        print('illust.id: %s sending failed after 2 retrys' % illust.id)
+                    sleep(1)
+    # after sending one image, clear file_id
+    file_id = None
 
 
-def send_photo(path, chat_id, caption=None):
+def get_file_id(jsonstr):
+    json_rst = json.loads(jsonstr)
+    file_id = json_rst['result']['photo'][0]['file_id']
+    return file_id
+
+
+def send_photo(path, chat_id, caption=None, mode='with_file', file_id=None):
     url = 'https://api.telegram.org/bot%s/sendPhoto' % (c.tg_bot_token)
     data = {
         'chat_id': chat_id,
         'caption': caption,
         'parse_mode': 'MarkdownV2'
     }
-    with open(path, 'rb') as photo:
+    # sending with file
+    if mode == 'with_file':
+        with open(path, 'rb') as photo:
+            response_text = str()
+            try:
+                response = requests.post(url, data=data, files={'photo': photo})
+                response_text = response.text
+                response.raise_for_status()
+                if response.status_code == 200:
+                    file_id = get_file_id(response.text)
+                    return {'status': True, 'file_id': file_id}
+            except requests.RequestException as err:
+                print('Send to telegram error! '+str(err))
+                print(response_text)
+                return {'status': False, 'file_id': None}
+    # sending with file_id
+    elif mode == 'with_id':
         response_text = str()
         try:
-            response = requests.post(url, data=data, files={'photo': photo})
+            data['photo'] = file_id
+            response = requests.post(url, data=data)
             response_text = response.text
             response.raise_for_status()
             if response.status_code == 200:
-                return True
+                file_id = get_file_id(response.text)
+                return {'status': True, 'file_id': file_id}
         except requests.RequestException as err:
             print('Send to telegram error! '+str(err))
             print(response_text)
-            return False
+            return {'status': False, 'file_id': None}
 
 
 if __name__ == '__main__':
